d8efad0d164f73d05ed588d226e98d79dd0561d2
Raghu Betina <rbetina@gmail.com>
2013-06-05 16:34:32 -0500

    Handled callback. Exchanging code for access token.

---
+++ app/controllers/auth_controller.rb
@@ -0,0 +1,14 @@
+require 'open-uri'
+
+class AuthController < ApplicationController
+  def facebook
+    @code = params[:code]
+
+    url = "https://graph.facebook.com/oauth/access_token?client_id=1375153749364030&redirect_uri=http://localhost:3000/auth/facebook&client_secret=73f323db75506823171bb405183b9c17&code=#{@code}"
+
+    @result = open(url).read
+
+    # Write some Ruby to extract the access token from that string and save it into the database under the currently signed in user (there is a column in the users table called 'facebook_access_token').
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Sandbox::Application.routes.draw do
   root to: 'Users#index'
 
+  get '/auth/facebook' => 'Auth#facebook'
+
   resource :session, only: [:new, :create, :destroy]
 
   resources :users
