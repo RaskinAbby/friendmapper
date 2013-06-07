81287933f5bc7ffd014342d6b63c91d5aaf93969
Raghu Betina <rbetina@gmail.com>
2013-06-07 14:18:02 -0500

    Updated app secret

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -2,15 +2,19 @@ require 'open-uri'
 
 class AuthController < ApplicationController
   def facebook
-    @code = params[:code]
+    code = params[:code]
 
-    url = "https://graph.facebook.com/oauth/access_token?client_id=1375153749364030&redirect_uri=http://localhost:3000/auth/facebook&client_secret=73f323db75506823171bb405183b9c17&code=#{@code}"
+    app_id = "1375153749364030"
+    redirect_uri = "http://localhost:3000/auth/facebook"
+    app_secret = "594004c7fda93757d15612b7a083dfa2"
 
-    @result = open(url).read
+    url = "https://graph.facebook.com/oauth/access_token?client_id=#{app_id}&redirect_uri=#{redirect_uri}&client_secret=#{app_secret}&code=#{code}"
+
+    result = open(url).read
 
     # Write some Ruby to extract the access token from that string and save it into the database under the currently signed in user (there is a column in the users table called 'facebook_access_token').
 
-    token = @result.split("&").first.split("=").last
+    token = result.split("&").first.split("=").last
 
     u = User.find_by_id(session[:user_id])
 
